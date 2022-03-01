802720aabaaf05479985785ab4525a554b38f768
Gowresh N <gowreshantha@gmail.com>
2022-03-01 13:27:02 +0530

    Nullable Types and Null Coalescing Opertaor

--- Program.cs
+++ Program.cs
@@ -14,5 +14,25 @@
         Console.WriteLine("Minimum Value float : " + float.MinValue + " Maximum Value float : " + float.MaxValue);
         #endregion
 
+        #region Nullable Types
+        /*
+         * In C# types are divided into 2 categories
+           Value Types - int, float, double, structs, enums etc.
+           Reference Types - Interface, Class, delegates, arrays etc.
+
+           By default value types are non nullable. To make them nullable use ?
+           int i = 0 (i is non nullable, i cannot be set to null, i = null will generate compiler error)
+           int? j = 0 (is is nullable int, so j=null is legal)
+
+           Nullable types brdge the diff b/w C# types and Database types
+            
+           Null Coalescing Opertaor:
+         */
+        int? TotalTickets = 100, Total = null;
+        int AvailableTickets = TotalTickets ?? 0;
+        int Totals = Total ?? 0;
+        Console.WriteLine("AvailableTickets = {0} and Totals = {1} ",AvailableTickets,Totals);
+        #endregion
+
     }
 }
