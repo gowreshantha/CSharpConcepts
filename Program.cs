669b48fc3d04db7342c382673a4bbfe425b11f51
Gowresh N <gowreshantha@gmail.com>
2022-03-01 12:52:10 +0530

    Bulit-in types

--- Program.cs
+++ Program.cs
@@ -2,7 +2,15 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Gowresh!");
-        Console.WriteLine("InProgress Branch");
+        #region Built-in types
+        /*
+         * Boolean type - Only true or false
+         * Integral types - sbyte, byte, short, ushort, int, unit, long, ulong, char
+         * Floating types - float and double
+         * Decimal types
+         * String type
+         */
+        #endregion
+     
     }
 }
